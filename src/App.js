fcdf144c27677e2bb908f388400ed8f4c0ecb03a
mackenziethompson157-art <mackenzie.thompson157@gmail.com>
2025-11-19 18:32:03 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-mport React, { useState, useEffect, useRef, useMemo } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { Heart, MessageCircle, Send, Search, Home, PlusSquare, User, ArrowLeft, ChevronLeft, ChevronRight, LogOut, Camera } from 'lucide-react';
 
 const SUPABASE_URL = 'https://emcnnvxvwmkmuudxbtqp.supabase.co';
